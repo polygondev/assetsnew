d3d54d425a79317c945b3987ea5749162430d570
Polygon <66850658+polygondev@users.noreply.github.com>
2024-02-07 22:42:08 +1100

    modified:   misc/installjre.sh

--- misc/installjre.sh
+++ misc/installjre.sh
@@ -1,6 +1,6 @@
 echo "installer script for java made by warfburg at 10:37pm"
 cd ~/Downloads
-wget 'https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_macos-x64_bin.tar.gz'
+curl 'https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_macos-x64_bin.tar.gz'
 tar -xf openjdk-21.0.2_macos-x64_bin.tar.gz -C $HOME/OpenJDK
 cat >> .zshrc
 export JAVA_HOME=$HOME/OpenJDK/jdk-21.0.1.jdk/Contents/Home
